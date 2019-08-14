1960e86207a14c67b9c526c85012963deabf4a18
chris48s <chris.shaw480@gmail.com>
2019-08-14 11:15:23 +0100

    update test bootstrap for new django versions

--- runtests.py
+++ runtests.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import django
 from django.conf import settings
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+root = lambda *x: os.path.join(BASE_DIR, *x)
 
 settings.configure(
     DEBUG=True,
@@ -17,11 +21,33 @@ settings.configure(
         'django.contrib.admin',
         'django.contrib.auth',
         'django.contrib.contenttypes',
+        'django.contrib.messages',
         'django.contrib.sessions',
         'django.contrib.sites',
         'sorl.thumbnail',
         'images',
-    )
+    ),
+    MIDDLEWARE=[
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+    ],
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'APP_DIRS': True,
+            'DIRS': [
+                root('templates'),
+            ],
+            'OPTIONS': {
+                'debug': True,
+                'context_processors': [
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        }
+    ],
 )
 
 if django.VERSION[:2] >= (1, 7):
